39cae201cdea5968f4a6bc5aa6ac2eb109337c1c
TheoCou <91134816+TheoCou@users.noreply.github.com>
2023-04-05 17:50:42 +0200

    modified:   src/log.inc.php

--- src/log.inc.php
+++ src/log.inc.php
@@ -3,12 +3,10 @@
 try { // connect to MySQL
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
     $bdd = new PDO('mysql:host=localhost;dbname=gmail', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', $pdo_options));
+    $reponse = $bdd->query('SELECT login, password FROM  inscription Where ');
+} catch(){
 
-    $reponse = $bdd->query('SELECT login, password FROM  inscription limit 1');
-
-    while ($data = $reponse->fetch()) {
-        $data['login'];
-        $data['password'];
+}
 
         //test form
         if (isset($_POST['login']) && isset($_POST['password'])) {
@@ -20,7 +18,10 @@ try { // connect to MySQL
                 echo "<p class=\"warning\">Vous avez oubliez votre mail ou password?</p>";
             }
             //compare login and password to the database
-            elseif ($login != $data['login'] && $mdp != $data['password']) {
+            else (password_verify($mdp, )){
+
+            }
+            /*elseif ($login != $data['login'] && $mdp != $data['password']) {
                 echo"<p class=\"warning\">Erreur login ou mot de passe?</p>";
             } else {
                 //valid connection
@@ -28,10 +29,10 @@ try { // connect to MySQL
                 echo "<p class=\"success\">Votre login est " . $_SESSION['nom'] . "
                             votre mot de passe est  " . md5($mdp);
                 header("Location: connection.php");
-                exit;
+                exit;*/
             }
-        }
-    }
+        
+    
     $reponse->closeCursor();
 } catch (Exception $e) {
     die("Erreur de connexion : " . $e->getMessage());
